84f3a5ca87dc2c96a3244574a4f908f62c5a0493
samulinukala <71631113+samulinukala@users.noreply.github.com>
2021-09-30 12:29:17 +0300

    added balls

---
+++ Assets/circlingScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circlingScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector2 Center;
+    public float RotateSpeed = 5f;
+    public float changingRadius;
+    public float RadiusMultiplier = 1f;
+    private float angle;
+    public float rotationOffset;
+    public float changeSpeed;
+
+    private void Start()
+    {
+        angle = rotationOffset;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        changingRadius=Mathf.Sin(Time.time * changeSpeed)*RadiusMultiplier;
+       Center = enemy.transform.position;
+        angle += RotateSpeed * Time.deltaTime;
+        var offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * changingRadius;
+        transform.position = Center + offset;
+
+    }
+}
